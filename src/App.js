107ac2a2cc99ee70a79a720592da88e92292f942
Aniket Mane <66128692+Mane-Aniket@users.noreply.github.com>
2024-11-27 09:45:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Background from './components/ui-components/background'
+function App() {
+  return (
+    <>
+    <Background />
+    </>
+  );
+}
+
+export default App;
